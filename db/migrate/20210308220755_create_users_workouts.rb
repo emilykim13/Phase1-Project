3ebc4e2b9dd09c1fffb5c307c18f06f78e539caf
github username <github email address>
2021-03-09 10:17:14 -0600

    fixed spelling

--- db/migrate/20210308220755_create_users_workouts.rb
+++ db/migrate/20210308220755_create_users_workouts.rb
@@ -3,7 +3,7 @@ class CreateUsersWorkouts < ActiveRecord::Migration[6.1]
     create_table :users_workouts do |t|
       t.string :name
       t.integer :users_id
-      t.integer :worksout_id
+      t.integer :workouts_id
       t.integer :total_duration
       t.string :favorites
       t.integer :date
