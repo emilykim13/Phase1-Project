d36f6224a7bda4d2cd0f97aa6cd3d9a14a112935
github username <github email address>
2021-03-10 14:56:52 -0600

    roberts give_exercises base

--- app/models/ab.rb
+++ app/models/ab.rb
@@ -1,11 +1,6 @@
 class Ab < ActiveRecord::Base
     belongs_to :workout
 
-
-    # def create_ab
-    #     Ab.create(name, intensity, duration)
-    # end
-
     def give_ab_id
         self.id
     end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -6,4 +6,17 @@ class Workout < ActiveRecord::Base
     has_many :legs
     has_many :shoulders
     has_many :backs
-end
+    def give_exercises
+        abs_exercise = all.select{|object| self.abs_id == object.abs_id} 
+        legs_exercise = all.select{|object| self.legs_id == object.legs_id}
+        shoulders_exercise = all.select{|object| self.abs_id == object.shoulders_id}
+        backs_exercise = all.select{|object| self.abs_id == object.abs_id}
+        arms_exercise = all.select{|object| self.arms_id == object.arms_id}
+        puts  "Your 7th grade gym teacher Mr.Willson is about to be so proud of you! This workout has an ab exercise called #{abs_exercise.name}, it is a #{abs_exercise.intensity} intensity exercise that lasts for #{ab_exercise.duration} minute(s). Let's get going!
+        There is only one thing we say to skipping leg day. Not today. This workout is a leg exercise called #{legs_exercise.name}, it is a #{legs_exercise.intensity} intensity exercise that lasts for #{ab_exercise.duration} minute(s). Woof!
+        It's about to be summer so we can't forget those shoulders. This workout is a shoulder exercise called #{shoulders_exercise.name}, it is a #{shoulders_exercise.intensity} intensity exercise that lasts for #{shoulders_exercise.duration}
+        Glad to see you back, and ready for more! This workout is a back exercise called #{backs_exercise.name}, it is a #{backs_exercise.intensity} intensity exercise that lasts for #{backs_exercise.new_duration}.
+        This workout has an #{arms_exercise.name}, it is a #{arms_exercise.intensity} intensity exercise that lasts for #{ab_exercise.duration} minute(s)."
+     end
+ end
+
