ded4bca5cced4a14139570662792ff511e0e3107
github username <github email address>
2021-03-11 16:47:45 -0600

    menu added to post workout

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,6 @@ require 'pry'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+ActiveRecord::Base.logger = nil
 require_all 'lib'
 require_all 'app'
